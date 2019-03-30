366f6a799da03143cbcb1fad41440da6cf3ebc2c
Jack Arthur Harrhy <me@jackharrhy.com>
2019-03-30 11:41:18 -0230

    Post based on game date instead of raw time remaining

--- remind.py
+++ remind.py
@@ -15,21 +15,16 @@ points_icon = 'https://webdiplomacy.net/images/icons/points.png'
 base_url =  'https://webdiplomacy.net'
 game_url = '{}/board.php?gameID={}'.format(base_url, game_id)
 
-last_time = 0
+last_game_date = ""
 
 while True:
     r = requests.get(game_url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    time_remain_span = soup.find_all('span', class_='timeremaining')[0]
-    unixtime = int(time_remain_span.get('unixtime'))
-    unixtimefrom = int(time_remain_span.get('unixtimefrom'))
+    game_date = soup.find_all('span', class_='gameDate')[0].string
 
-    seconds = unixtime - unixtimefrom
-    hours = (seconds / 60) / 60
-
-    if last_time < hours:
-        last_time = hours
+    if not last_game_date == game_date:
+        last_game_date == game_date
 
         game_name = soup.find_all('span', class_='gameName')[0].string
 
@@ -39,13 +34,17 @@ while True:
             timestamp='now'
             )
 
+        time_remain_span = soup.find_all('span', class_='timeremaining')[0]
+        unixtime = int(time_remain_span.get('unixtime'))
+        unixtimefrom = int(time_remain_span.get('unixtimefrom'))
+        seconds = unixtime - unixtimefrom
+        hours = (seconds / 60) / 60
         embed.add_field(name='Next', value='{:.2f}hrs'.format(hours))
 
         big_map_img_url_postfix  = soup.find(id='LargeMapLink').get('href')
         big_map_img = '{}/{}'.format(base_url, big_map_img_url_postfix)
         embed.set_image(big_map_img)
 
-        game_date = soup.find_all('span', class_='gameDate')[0].string
         game_pot = soup.find_all('span', class_='gamePot')[0].get_text()
         footer_text = "Pot: {}, {}".format(game_pot, game_date)
         embed.set_footer(text=footer_text, icon_url=points_icon)
